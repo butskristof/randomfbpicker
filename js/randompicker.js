d7b3cd99a886f2db3e2454186a440f3c039cbf19
Kristof Buts <github@kristofbuts.be>
2018-03-25 18:35:54 +0200

    v1 done

--- js/randompicker.js
+++ js/randompicker.js
@@ -10,8 +10,22 @@ function picker() {
         requesturl,
         {"access_token": formdata[0].value},
         function (data) {
-            // console.log(data);
+            let nrOfWinners = parseInt(formdata[2].value);
+            var winningcomments = [];
 
+            for (var i = 0; i < nrOfWinners; ++i) {
+                let number = Math.round( Math.random() * data.data.length );
+                winningcomments.push(data.data[number]);
+            }
+
+            console.log(winningcomments);
+
+            winningcomments.forEach(function (comment) {
+                console.log(comment.from.name);
+                $("#winnerlist").append("<li>" + comment.from.name + "</li>");
+            });
+
+            $("#result").show();
         }
     );
 
